7e313cb621292759a7784f7d3632714a273363fb
blueverves <volker.rabe@gmail.com>
2016-10-11 09:53:10 +0100

    add parameter validation

--- lib/bitmap_image.rb
+++ lib/bitmap_image.rb
@@ -1,6 +1,9 @@
 class BitmapImage
 
+  class Error < ArgumentError; end
+
   def initialize(w, h)
+    validate_dimensions(w, h)
     @width = w
     @height = h
     clear
@@ -11,14 +14,17 @@ class BitmapImage
   end
 
   def color_pixel(x,y,c)
+    validate_pixel(x, y)
     @pixels[y-1][x-1] = c
   end
 
   def draw_vertical(x, y1, y2, c)
+    validate_vertical(x, y1, y2)
     (y1..y2).each{ |y| @pixels[y-1][x-1] = c }
   end
 
   def draw_horizontal(x1, x2, y, c)
+    validate_horizontal(x1, x2, y)
     (x1..x2).each{ |x| @pixels[y-1][x-1] = c }
   end
 
@@ -28,4 +34,29 @@ class BitmapImage
     end
   end
 
+  private
+
+  def validate_dimensions(w, h)
+    if w < 1 || h < 1
+      raise BitmapImage::Error.new("can't create image. Width and height must be at least 1")
+    end
+  end
+
+  def validate_pixel(x, y)
+    if y > @height || x > @width || y < 1 || x < 1
+      raise BitmapImage::Error.new("can't color pixel. Coordinates don't exist")
+    end
+  end
+
+  def validate_vertical(x, y1, y2)
+    if y1 > @height || y2 > @height || y1 > y2 || x > @width || y1 < 1 || y2 < 1 || x < 1
+      raise BitmapImage::Error.new("can't draw vertical. Coordinates are not valid")
+    end
+  end
+
+  def validate_horizontal(x1, x2, y)
+    if y > @height || x1 > @width || x2 > @width || x1 > x2 || y < 1 || x1 < 1 || x2 < 1
+      raise BitmapImage::Error.new("can't draw horizontal. Coordinates are not valid")
+    end
+  end
 end
--- spec/bitmap_image_spec.rb
+++ spec/bitmap_image_spec.rb
@@ -78,6 +78,47 @@ describe BitmapImage do
                              "O O O V O O\n")
       end
     end
+
+
+  end
+
+  context "Draw an invalid image" do
+    let(:image) { BitmapImage.new(3,4) }
+
+    describe "#new" do
+      it "raises a BitmapImage error" do
+        message = "can't create image. Width and height must be at least 1"
+        expect{ BitmapImage.new(0,5) }.to raise_error(BitmapImage::Error, message)
+      end
+    end
+
+    describe "#color_pixel" do
+      it "raises a BitmapImage error" do
+        message = "can't color pixel. Coordinates don't exist"
+        expect{ image.color_pixel(4,4,"C") }.to raise_error(BitmapImage::Error, message)
+        expect{ image.color_pixel(3,5,"C") }.to raise_error(BitmapImage::Error, message)
+      end
+    end
+
+    describe "#draw_vertical" do
+      it "raises a BitmapImage error" do
+        message = "can't draw vertical. Coordinates are not valid"
+        expect{ image.draw_vertical(4,1,2,"V") }.to raise_error(BitmapImage::Error, message)
+        expect{ image.draw_vertical(2,0,2,"V") }.to raise_error(BitmapImage::Error, message)
+        expect{ image.draw_vertical(1,1,5,"V") }.to raise_error(BitmapImage::Error, message)
+        expect{ image.draw_vertical(1,2,1,"V") }.to raise_error(BitmapImage::Error, message)
+      end
+    end
+
+    describe "#draw_horizontal" do
+      it "raises a BitmapImage error" do
+        message = "can't draw horizontal. Coordinates are not valid"
+        expect{ image.draw_horizontal(5,1,2,"H") }.to raise_error(BitmapImage::Error, message)
+        expect{ image.draw_horizontal(0,3,2,"H") }.to raise_error(BitmapImage::Error, message)
+        expect{ image.draw_horizontal(1,3,5,"H") }.to raise_error(BitmapImage::Error, message)
+        expect{ image.draw_horizontal(3,1,2,"H") }.to raise_error(BitmapImage::Error, message)
+      end
+    end
   end
 
   private
